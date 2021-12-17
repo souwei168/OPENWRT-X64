59ce2c49d68c6b5e1a74a546b723879cf7be0342
CNCTION <48990279+souwei168@users.noreply.github.com>
2021-12-17 19:26:59 +0800

    Update diy-part02.sh

--- diy-part02.sh
+++ diy-part02.sh
@@ -12,9 +12,6 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
-
-
-#git clone https://github.com/linkease/istore.git package/istore
 #sirpdboy
 #git clone https://github.com/sirpdboy/sirpdboy-package.git package/sirpdboy-package
 #git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
@@ -44,6 +41,7 @@ git clone https://github.com/kiddin9/luci-app-xlnetacc.git package/luci-app-xlne
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
 git clone https://github.com/riverscn/openwrt-iptvhelper.git package/openwrt-iptvhelper
+#git clone https://github.com/linkease/istore.git package/istore
 #添加smartdns
 git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
