43601e94ddd027e0c486494e925f9d6dee62c8a9
CNCTION <48990279+souwei168@users.noreply.github.com>
2021-12-18 20:34:35 +0800

    Update diy-part02.sh

--- diy-part02.sh
+++ diy-part02.sh
@@ -13,15 +13,15 @@
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 # 修改输出文件名
-#sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=I225-$(shell date +%Y%m%d)-$(VERSION_DIST_SANITIZED)/g' include/image.mk
+sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=I225-$(shell date +%Y%m%d)-$(VERSION_DIST_SANITIZED)/g' include/image.mk
 #sirpdboy
-#git clone https://github.com/sirpdboy/sirpdboy-package.git package/sirpdboy-package
-#git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
-#git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
-#git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
-#git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
-#git clone https://github.com/sirpdboy/luci-app-poweroffdevice.git package/luci-app-poweroffdevice
-#git clone https://github.com/sirpdboy/luci-app-autotimeset.git package/luci-app-autotimeset
+git clone https://github.com/sirpdboy/sirpdboy-package.git package/sirpdboy-package
+git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
+git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
+git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
+git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
+git clone https://github.com/sirpdboy/luci-app-poweroffdevice.git package/luci-app-poweroffdevice
+git clone https://github.com/sirpdboy/luci-app-autotimeset.git package/luci-app-autotimeset
 
 
 #修正连接数（by ベ七秒鱼ベ）
