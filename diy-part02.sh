0e167d0e777fb4d6a1b94091daa0d6ed41d83aec
CNCTION <48990279+souwei168@users.noreply.github.com>
2021-12-18 10:17:43 +0800

    Update diy-part02.sh

--- diy-part02.sh
+++ diy-part02.sh
@@ -44,6 +44,8 @@ git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
 git clone https://github.com/riverscn/openwrt-iptvhelper.git package/openwrt-iptvhelper
 #git clone https://github.com/linkease/istore.git package/istore
+git clone https://github.com/ozon/luci-app-ttyd.git package/luci-app-ttyd
+git clone https://github.com/Porcat/luci-app-filetransfer.git package/luci-app-filetransfer
 #添加smartdns
 git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
